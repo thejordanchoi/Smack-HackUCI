1bbe58129e0427379491fec2666b0e15eb6d23f8
Jordan Choi <jordac6@uci.edu>
2019-02-16 22:44:54 -0800

    Intial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+
+    const db = firebase.firestore();
+
+    const test = db.collection('event').doc('PXANvk3hdxz59Vy743oF');
+
+    
+
+});
